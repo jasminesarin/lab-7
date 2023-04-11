85ec035c37f61977824fd9e427e93e433f69b837
Jasmine Sarin <66775431+jasminesarin@users.noreply.github.com>
2023-04-11 16:01:17 -0500

    added variables and created a click event

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
+
+//Adding a console log event to tell us that the page has loaded
 function onLoadEvent() {
   console.log("The page has finished loading!");
 }
-
 window.addEventListener("load", onLoadEvent);
+
+// references to the button, text and output elements
+function init(){
+var button = document.getElementById("entrybutton");
+var output = document.getElementById("textoutput");
+var entryinput= document.getElementById("textentry");
+
+  // Define a function to handle button clicks
+  function handleClick() {
+    // Get the value of the input element
+    const inputValue = entryinput.value;
+    // Set the text content of the output element
+    output.innerText = `You entered: ${inputValue}`;
+  }
+}
+// Add an event listener to the button
+button.addEventListener("click", handleClick);
+window.addEventListener('load', init);
