dc081ff6ce32d64a3c81ce0403611740bd1c2848
Jasmine Sarin <66775431+jasminesarin@users.noreply.github.com>
2023-04-11 22:24:02 -0500

    correcting some syntax and trying again

--- js/script.js
+++ js/script.js
@@ -3,24 +3,27 @@
 function onLoadEvent() {
   console.log("The page has finished loading!");
 }
-window.addEventListener("load", onLoadEvent);
+window.addEventListener('load', onLoadEvent);
 
 //making alert me as a button
 // Get a reference to the entry button and input element
-var button = document.getElementById("entrybutton");
+var button = document.getElementById('entrybutton');
 var inputText = document.getElementById('entryinput');
 
 // Define a function to handle button clicks
 function handleClick() {
   
-  //create a text input variable
+  //create a constant to catch the value of inputText variable
   const showText = inputText.value;
+  
+  //
   
   // Set the text content of the show results element
   showResults.innerText = `Jasmine Sarin: ${inputValue}`;
+  
 }
 
 // Add an event listener to the entry button
-entryButton.addEventListener("click", handleClick);
+entryButton.addEventListener('click', handleClick);
 
 window.addEventListener('load', init);
